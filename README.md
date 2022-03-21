791bf2f69b3caf712b89fa55a5c6c5f91820a537
Jonas Galvez <jonasgalvez@gmail.com>
2022-03-20 21:05:23 -0300

    minimon

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # minimon
+
+A minimal Node.js restarter based on [zx](https://github.com/google/zx).
+
+A modest [`nodemon`](https://github.com/remy/nodemon) replacement.
+
+Check out [the blog post covering how it was written](https://hire.jonasgalvez.com.br/2022/mar/20/building-your-own-nodemon).
+
+## Install
+
+```bash
+npm i minimon -g
+```
+
+## Usage
+
+```bash
+minimon app.js
+```
+
+## Configuration
+
+Just create `minimon.conf` and override the defaults. JSON5 is accepted.
+
+```js
+{
+  watch: ['**/*.mjs', '**/*.js'],
+  ignored: ['**/node_modules/**'],
+}
+```
---
+++ minimon.mjs
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+import JSON5 from 'json5'
+import chokidar from 'chokidar'
+import { registerGlobals } from './zx.mjs'
+import { EventEmitter } from 'events'
+
+registerGlobals()
+
+let node
+
+const ee = new EventEmitter() 
+const config = await loadConfig()
+
+chokidar
+  .watch(config.watch, {
+    ignoreInitial: true,
+    ignored: config.ignored,
+  })
+  .on('all', () => {
+    ee.emit('restart')
+  })
+
+ee.on('restart', async () => {
+  node.catch(() => start())
+  node.kill()
+  console.log('ℹ restarting')
+})
+
+await start()
+
+async function start () {
+  node = $`${
+    process.argv[0]
+  } ${
+    process.argv.slice(2).map($.originalQuote).join(' ')
+  }`
+  try {
+    console.log('ℹ started')
+    await node
+  } catch {
+    // No need to do anything as zx will print the original stderr
+  }
+}
+
+async function loadConfig () {
+  const defaults = {
+    watch: ['**/*.mjs', '**/*.js'],
+    ignored: ['**/node_modules/**'],
+  }
+  if (await fs.exists('./minimon.conf')) {
+    return Object.assign(defaults, JSON5.parse(await fs.readFile('./minimon.conf', 'utf8')))
+  } else {
+    return defaults
+  }
+}
